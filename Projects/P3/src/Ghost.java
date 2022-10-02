30a2e1f2ef95d0bf0ac0a8ffd44780f9fc518e22
Brandon <brandongormley1217@gmail.com>
2022-10-01 20:41:44 -0400

    Implemented ghost-attack

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -24,6 +24,27 @@ public class Ghost {
   }
 
   public boolean attack() {
-    return false;
+    if (is_pacman_in_range()) {
+    	int x = myLoc.x;
+        int y = myLoc.y;
+        Location pacman_loc;
+        if (myMap.getLoc(new Location(x-1, y)).contains(Map.Type.PACMAN)) {
+        	pacman_loc = new Location(x-1, y);
+        } else if (myMap.getLoc(new Location(x+1, y)).contains(Map.Type.PACMAN)) {
+			pacman_loc = new Location(x+1, y);
+        } else if (myMap.getLoc(new Location(x, y-1)).contains(Map.Type.PACMAN)) {
+        	pacman_loc = new Location(x, y-1);
+        } else if (myMap.getLoc(new Location(x, y+1)).contains(Map.Type.PACMAN)) {
+        	pacman_loc = new Location(x, y+1);
+        } else {
+        	//If there was a problem, and pac-man cannot be found in the ghost's range.
+        	return false;
+        }
+    	myMap.move(myName, pacman_loc, Map.Type.GHOST);    	
+    	return true;
+    }
+    else {
+    	return false;
+    }
   }
 }
