cf3e2331dc896363af8917d2fee37b8c9cb2e4cb
Brian Hopkins <brianhop@terpmail.umd.edu>
2022-10-04 22:27:17 -0400

    finished junit, but cant test it until all code is done

--- Projects/P3/tests/TestPacManInRange.java
+++ Projects/P3/tests/TestPacManInRange.java
@@ -1,9 +1,21 @@
+import java.awt.Color;
 import java.io.*;
+import org.junit.Test;
 import junit.framework.*;
 
 public class TestPacManInRange extends TestCase {
-
+  @Test
   public void testPacManInRange() throws FileNotFoundException {
-    return null;
+	  NoFrame frame = new NoFrame();
+	  
+	  PacMan pacman = frame.addPacMan(new Location(2,3));
+	  //ghost1 is diagonal from pacman so this should be false
+	  Ghost ghost1 = frame.addGhost(new Location(1,2), "Blinky", Color.red);
+	  assertFalse(ghost1.is_pacman_in_range());
+	  //ghost2 is right above pacman, so this should return true for ghost2
+	  //but still be false for ghost1
+	  Ghost ghost2 = frame.addGhost(new Location(1,3), "Clyde", Color.orange);
+	  assertFalse(ghost1.is_pacman_in_range());
+	  assertTrue(ghost2.is_pacman_in_range());
   }
 }
