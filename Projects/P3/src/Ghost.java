3e9be48b2689e5308cfdaa8155a4267c9fc6ff7c
Maxwell Myers <mmyers12@umd.edu>
2022-10-05 21:51:09 -0400

    Wall changes 4 ghost

--- Projects/P3/src/Ghost.java
+++ Projects/P3/src/Ghost.java
@@ -13,9 +13,31 @@ public class Ghost {
 
   public ArrayList<Location> get_valid_moves() {
     ArrayList<Location> lst = new ArrayList<Location>();
+    if(myMap.getLoc(new Location(myLoc.x, myLoc.y+1)).contains(Map.Type.WALL)){//down
+    lst.add(new Location(myLoc.x+1, myLoc.y)); // right
+    lst.add(new Location(myLoc.x-1, myLoc.y)); // left
+    lst.add(new Location(myLoc.x, myLoc.y-1)); // up
+    return lst;
+  }else if(myMap.getLoc(new Location(myLoc.x, myLoc.y-1)).contains(Map.Type.WALL)){//up
+    lst.add(new Location(myLoc.x+1, myLoc.y)); // right
+    lst.add(new Location(myLoc.x, myLoc.y+1)); // down
+    lst.add(new Location(myLoc.x-1, myLoc.y)); // left
+    return lst;
+  }else if(myMap.getLoc(new Location(myLoc.x+1, myLoc.y)).contains(Map.Type.WALL)){//right
+    lst.add(new Location(myLoc.x-1, myLoc.y)); // left
+    lst.add(new Location(myLoc.x, myLoc.y-1)); // up
+    lst.add(new Location(myLoc.x, myLoc.y+1)); // down
+    return lst;
+  }else if(myMap.getLoc(new Location(myLoc.x-1, myLoc.y)).contains(Map.Type.WALL)){//left
+    lst.add(new Location(myLoc.x, myLoc.y-1)); // up
+    lst.add(new Location(myLoc.x+1, myLoc.y)); // right
+    lst.add(new Location(myLoc.x, myLoc.y+1)); // down
+    return lst;
+  }
     lst.add(new Location(myLoc.x+1, myLoc.y));
-    lst.add(new Location(myLoc.x+1, myLoc.y+1));
+    lst.add(new Location(myLoc.x-1, myLoc.y));
     lst.add(new Location(myLoc.x, myLoc.y+1));
+    lst.add(new Location(myLoc.x, myLoc.y-11));
     return lst;
   }
 
