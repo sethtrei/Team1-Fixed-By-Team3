36a30e881721845894bb610cec730e81d31fe397
Maxwell Myers <mmyers12@umd.edu>
2022-10-05 22:02:42 -0400

    tests for pacman valid moves

--- Projects/P3/tests/TestPacManValidMoves.java
+++ Projects/P3/tests/TestPacManValidMoves.java
@@ -1,9 +1,18 @@
 import java.io.*;
+import java.util.ArrayList;
+
 import junit.framework.*;
 
 public class TestPacManValidMoves extends TestCase {
 
   public void testPacManValidMoves() throws FileNotFoundException {
-    return null;
+    MainFrame game = new MainFrame();
+    game.addPacMan(new Location(1, 1));
+    PacMan kaneki = new PacMan("kaneki", new Location(1,1), game.getMap());
+    ArrayList<Location> lst = kaneki.get_valid_moves();
+    assertTrue(lst.contains(new Location(1, 2)));
+    assertTrue(lst.contains(new Location(2, 1)));
+    assertFalse(lst.contains(new Location(0,0)));
+    
   }
 }
