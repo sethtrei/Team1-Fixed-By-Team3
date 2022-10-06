e315d60775e51864dbb8197bbaab509764bbc143
Brandon <brandongormley1217@gmail.com>
2022-10-05 20:26:16 -0400

    Added test for map-eatCookie

--- Projects/P3/tests/TestMapEatCookie.java
+++ Projects/P3/tests/TestMapEatCookie.java
@@ -1,9 +1,17 @@
+import java.awt.Color;
 import java.io.*;
 import junit.framework.*;
 
 public class TestMapEatCookie extends TestCase {
 
   public void testMapEatCookie() throws FileNotFoundException {
-    return null;
+	  NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+	  //Creating Players
+	  Ghost ghost = frame.addGhost(new Location(1, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
+	  PacMan pacman = frame.addPacMan(new Location(1, 3)); //Creates PacMan at location x, y
+	  
+	  //There should already be a cookie underneath eatCookie so consume() should be non-null
+	  assertNotNull(frame.getMap().eatCookie("pacman"));
   }
 }
