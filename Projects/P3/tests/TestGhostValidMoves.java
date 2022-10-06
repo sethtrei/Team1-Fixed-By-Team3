6f550a9bd76ad0b7fa819ff57e4e58beb7b52aae
Maxwell Myers <mmyers12@umd.edu>
2022-10-05 22:06:05 -0400

    added tests for ghost valid moves

--- Projects/P3/tests/TestGhostValidMoves.java
+++ Projects/P3/tests/TestGhostValidMoves.java
@@ -1,9 +1,16 @@
+import java.awt.Color;
 import java.io.*;
 import junit.framework.*;
 
 public class TestGhostValidMoves extends TestCase {
 
   public void testGhostValidMoves() throws FileNotFoundException {
-    return null;
+    MainFrame game = new MainFrame();
+    game.addGhost(new Location(1, 1), "Blinky", Color.cyan);
+    Ghost kaneki = new Ghost("kaneki", new Location(1, 1), game.getMap());
+    ArrayList<Location> lst = kaneki.get_valid_moves();
+    assertTrue(lst.contains(new Location(1, 2)));
+    assertTrue(lst.contains(new Location(2, 1)));
+    assertFalse(lst.contains(new Location(0,0)));
   }
-}
+} 
