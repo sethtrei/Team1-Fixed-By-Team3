489b669708296bbb25b6213688e7219710434bb9
matthew-day <112720668+matthew-day@users.noreply.github.com>
2022-10-05 22:00:42 -0400

    Update TestMapGetLoc.java

--- Projects/P3/tests/TestMapGetLoc.java
+++ Projects/P3/tests/TestMapGetLoc.java
@@ -5,8 +5,8 @@ public class TestMapGetLoc extends TestCase {
 
   public void testMapGetLoc() throws FileNotFoundException {
     NoFrame frame = new NoFrame();
-    PacMan pacman = frame.addPacMan(new Location(0, 0));
+    PacMan pacman = frame.addPacMan(new Location(1, 1));
     pacman.move();
-    assertFalse(frame.getMap().getloc((new Location(0, 0)) == Map.Type.PACMAN);
+    assertFalse(frame.getMap().getloc((new Location(1, 1)) == Map.Type.PACMAN);
   }
 }
