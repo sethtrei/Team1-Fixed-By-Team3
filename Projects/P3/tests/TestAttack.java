83061a79d28a2b1bac0748138f6e4611c58ba9f9
Brandon <brandongormley1217@gmail.com>
2022-10-05 20:15:37 -0400

    Added test for ghost-attack

--- Projects/P3/tests/TestAttack.java
+++ Projects/P3/tests/TestAttack.java
@@ -1,9 +1,17 @@
 import java.io.*;
 import junit.framework.*;
+import java.awt.Color;
 
 public class TestAttack extends TestCase {
 
   public void testAttack() throws FileNotFoundException {
-    return null;
+	  NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
+
+	  //Creating Players
+	  Ghost ghost = frame.addGhost(new Location(1, 2), "name", Color.red); //Creates a red ghost named "name" at location x,y
+	  PacMan pacman = frame.addPacMan(new Location(1, 3)); //Creates PacMan at location x, y
+	  
+	  //Since the ghost and pacman are next to eachother, this should return True.
+	  assertTrue(ghost.attack());
   }
 }
