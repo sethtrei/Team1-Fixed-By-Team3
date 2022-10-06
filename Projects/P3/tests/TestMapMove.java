45ab133daf2bd9b69700698fd35dd52fd0561571
Maxwell Myers <mmyers12@umd.edu>
2022-10-05 22:10:08 -0400

    Map tests

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -4,6 +4,11 @@ import junit.framework.*;
 public class TestMapMove extends TestCase {
 
   public void testMapMove() throws FileNotFoundException {
-    return null;
+    MainFrame game = new MainFrame();
+    game.addPacMan(new Location(1, 1));
+    PacMan kaneki = new PacMan("pacman", new Location(1,1), game.getMap());
+    game.getMap().move("pacman", new Location(2,2), Map.Type.PACMAN);
+    assertFalse(game.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN));
+    assertTrue(game.getMap().getLoc(new Location(2, 2)).contains(Map.Type.PACMAN));
   }
 }
