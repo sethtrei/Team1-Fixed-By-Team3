1aa907c25b18c4a6a67d3aea0dc5de21f8aeeb83
matthew-day <112720668+matthew-day@users.noreply.github.com>
2022-10-05 21:59:47 -0400

    Update TestPacManMove.java

--- Projects/P3/tests/TestPacManMove.java
+++ Projects/P3/tests/TestPacManMove.java
@@ -5,7 +5,7 @@ public class TestPacManMove extends TestCase {
 
   public void testPacManMove() throws FileNotFoundException {
     NoFrame frame = new NoFrame();
-    PacMan pacman = frame.addPacMan(new Location(0, 0));
+    PacMan pacman = frame.addPacMan(new Location(1, 1));
     pacman.move();
     assertFalse(frame.getMap().getloc((new Location(0, 0)) == Map.Type.PACMAN);
   }
