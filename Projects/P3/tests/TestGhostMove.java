c2abbcf55d77b7c68538f8572f4a721389de5813
matthew-day <112720668+matthew-day@users.noreply.github.com>
2022-10-05 20:00:01 -0400

    Update TestGhostMove.java

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -3,5 +3,10 @@ import junit.framework.*;
 
 public class TestGhostMove extends TestCase {
 
-  public void testGhostMove() throws FileNotFoundException {}
+  public void testGhostMove() throws FileNotFoundException {
+    NoFrame frame = new NoFrame();
+    Ghost = frame.addGhost(new Location(0, 0));
+    ghost.move();
+    assertFalse(frame.getMap().getloc((new Location(0, 0)) == Map.Type.GHOST);
+  }
 }
