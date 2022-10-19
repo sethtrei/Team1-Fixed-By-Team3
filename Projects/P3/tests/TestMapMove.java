812c1c6845491ef24d7c3bdf79da04c51a95250d
Maxwell Myers <mmyers12@umd.edu>
2022-10-18 21:54:44 -0400

    fixed test

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -7,8 +7,8 @@ public class TestMapMove extends TestCase {
     NoFrame game = new NoFrame();
     game.addPacMan(new Location(1, 1));
     PacMan kaneki = new PacMan("pacman", new Location(1,1), game.getMap());
-    game.getMap().move("pacman", new Location(2,2), Map.Type.PACMAN);
+    game.getMap().move("pacman", new Location(1,2), Map.Type.PACMAN);
     assertFalse(game.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN));
-    assertTrue(game.getMap().getLoc(new Location(2, 2)).contains(Map.Type.PACMAN));
+    assertTrue(game.getMap().getLoc(new Location(1, 2)).contains(Map.Type.PACMAN));
   }
 }
