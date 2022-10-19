31d2ed882b237a19a6f0104674e0c36043624a6c
Maxwell Myers <mmyers12@umd.edu>
2022-10-18 22:28:52 -0400

    fixed for loop

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -6,8 +6,8 @@ public class TestMapMove extends TestCase {
   public void testMapMove() throws FileNotFoundException {
     NoFrame game = new NoFrame();
     game.addPacMan(new Location(1, 1));
-    PacMan kaneki = new PacMan("pacman", new Location(1,1), game.getMap());    
-    game.getMap().move();
+    PacMan kaneki = new PacMan("pacman", new Location(1,1), game.getMap());
+    game.getMap().move("pacman", new Location(1,2), Map.Type.PACMAN);
     assertFalse(game.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN));
     assertTrue(game.getMap().getLoc(new Location(1, 2)).contains(Map.Type.PACMAN));
   }
