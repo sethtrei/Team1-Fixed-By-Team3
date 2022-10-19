7546ad8d72c69536ce727cc723c93a072b61b240
maxMyers13 <72959101+maxMyers13@users.noreply.github.com>
2022-10-18 23:13:32 -0400

    Update TestMapMove.java

--- Projects/P3/tests/TestMapMove.java
+++ Projects/P3/tests/TestMapMove.java
@@ -6,7 +6,6 @@ public class TestMapMove extends TestCase {
   public void testMapMove() throws FileNotFoundException {
     NoFrame game = new NoFrame();
     game.addPacMan(new Location(1, 1));
-    PacMan kaneki = new PacMan("pacman", new Location(1,1), game.getMap());
     game.getMap().move("pacman", new Location(2,2), Map.Type.PACMAN);
     assertFalse(game.getMap().getLoc(new Location(1, 1)).contains(Map.Type.PACMAN));
     assertTrue(game.getMap().getLoc(new Location(2, 2)).contains(Map.Type.PACMAN));
